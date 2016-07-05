a38ca696d1fabd1d52cd4aebb323cb265b3b605c
Kiarie1 <miringuk@gmail.com>
2016-07-05 13:43:14 -0400

    Update MainActivity.java

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -7,36 +7,39 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    int scoreTeamA = 0;
+    int scoreTeamB = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    int score = 0;
-
     // This method is called when the twoPoints button is clicked
-    public void threePoints (View v) {
-        score = (score + 3);
-        displayForTeamA(score);
+    public void threePointsA(View v) {
+        scoreTeamA = scoreTeamA + 3;
+        displayForTeamA(scoreTeamA);
     }
 
     // adds 2 points to score
-    public void twoPoints (View view) {
-        score = (score +2);
-        displayForTeamA(score);
+    public void twoPointsA(View view) {
+        scoreTeamA = scoreTeamA + 2;
+        displayForTeamA(scoreTeamA);
     }
 
     // adds 1 point to score
-    public void freePoint (View view) {
-        score = (score + 1);
-        displayForTeamA(score);
+    public void freePointA(View view) {
+        scoreTeamA = scoreTeamA + 1;
+        displayForTeamA(scoreTeamA);
     }
 
-    // resets score
-    public void reset (View view) {
-        score = (0);
-        displayForTeamA(score);
+    // resets scores of both teams
+    public void reset(View view) {
+        scoreTeamA = 0;
+        scoreTeamB = 0;
+        displayForTeamA(scoreTeamA);
+        displayForTeamB(scoreTeamB);
     }
 
     /**
@@ -44,6 +47,34 @@ public class MainActivity extends AppCompatActivity {
      */
     public void displayForTeamA(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    // This method is called when the twoPointsB button is clicked
+    public void threePointsB(View v) {
+        scoreTeamB = scoreTeamB + 3;
+        displayForTeamB(scoreTeamB);
+    }
+
+    // adds 2 points to Team B
+    public void twoPointsB(View view) {
+        scoreTeamB = scoreTeamB + 2;
+        displayForTeamB(scoreTeamB);
+    }
+
+    // adds 1 point to Team B
+
+    public void freePointB(View view) {
+        scoreTeamB = scoreTeamB + 1;
+        displayForTeamB(scoreTeamB);
+    }
+
+
+    /**
+     * Displays the given score for Team B.
+     */
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
         scoreView.setText(String.valueOf(score));
     }
 }
